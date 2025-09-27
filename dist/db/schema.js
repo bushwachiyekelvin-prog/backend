94ad14ed0467a3bcf6902af3f9d2a0d4910e3e7d
Kelvin Bush <kelybush@gmail.com>
2025-09-27 18:16:41 +0300

    remove dist from repo

--- dist/db/schema.js
+++
@@ -1,7 +0,0 @@
-export * from "./schema/users";
-export * from "./schema/personalDocuments";
-export * from "./schema/businessProfiles";
-export * from "./schema/businessDocuments";
-export * from "./schema/relations";
-export * from "./schema/investorOpportunities";
-export * from "./schema/investorOpportunityBookmarks";
